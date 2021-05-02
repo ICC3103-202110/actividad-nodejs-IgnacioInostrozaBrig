620779302bf83435cfbff061759d1f5f11b3b972
Ignacio Inostroza <iinostroza1@miuandes.cl>
2021-05-02 19:09:27 -0400

    Tarea Fibonacci ver0.1

--- TareaFibonacci.js
+++ TareaFibonacci.js
@@ -1,24 +1,48 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });  
+
 function fibo(number) {
     limit = number;
-    i = 0;
+    i = 1;
     a = 0;
     b = 1;
-
+    c = 0;
     if (number==0){
-        console.log(0);
+        console.log(`\nThe Fibonacci sequence for 0 is:`);
+        console.log(`F(0) = 0\n`);
     }
 
     else if (number==1){
-        console.log(0);
-        console.log(1);
+        console.log(`\nThe Fibonacci sequence for 1 is:`);
+        console.log(`F(1) = 1\n`);
     }
 
     else{
         while (i<limit){
-        console.log(number);
+        c = b;
+        b = a+b;
+        a = c;
         i++;
         }
+        console.log(`\nThe Fibonacci sequence for ${number} is:`);
+        console.log(`F(${number}) = ${b}\n`);
     }    
 }
+// Main code
 
-fibo(1);
+rl.question('\nInput a number: \n', (answer) => {
+    //If the input is invalid
+    if (isNaN(answer)) {
+        console.log('\nInvalid input, please try again\n');
+        rl.close();
+    }
+    //For fibonacci valid inputs
+    else{
+        fibo(answer);
+        rl.close();
+        }
+});
