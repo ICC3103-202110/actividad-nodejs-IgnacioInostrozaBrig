c35f87f27c2399e25784180b9fac77c9389cb2ab
Ignacio Inostroza <iinostroza1@miuandes.cl>
2021-05-02 18:22:43 -0400

    Tarea Fibonacci ver0.1

---
+++ TareaFibonacci.js
@@ -0,0 +1,24 @@
+function fibo(number) {
+    limit = number;
+    i = 0;
+    a = 0;
+    b = 1;
+
+    if (number==0){
+        console.log(0);
+    }
+
+    else if (number==1){
+        console.log(0);
+        console.log(1);
+    }
+
+    else{
+        while (i<limit){
+        console.log(number);
+        i++;
+        }
+    }    
+}
+
+fibo(1);
